9eeff9c96c6a42c2422306ec20e60ac10e106a99
Constantine Popov <popovca@yandex.ru>
2025-06-02 12:31:58 +0300

    Errors checked

--- src/main/java/ru/netology/springBootDemo/config/JavaConfig.java
+++ src/main/java/ru/netology/springBootDemo/config/JavaConfig.java
@@ -1,4 +1,4 @@
-package config;
+package ru.netology.springBootDemo.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
